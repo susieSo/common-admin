db550f2049729fa30833cedd0c3ad0b084093fd2
소수지 <susie0529@imform.co.kr>
2024-11-26 17:12:11 +0900

    프로젝트 모듈 구성 및 Config 파일 설정

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
-
 ## Getting Started
-
-First, run the development server:
 
 ```bash
 npm run dev
@@ -14,23 +10,70 @@ pnpm dev
 bun dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+---
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+## 모듈 및 라이브러리의 용도와 선택한 이유
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+- **Next**
+  - React 기반 / SSR
+- **shadcn/ui**
+  - Radix-ui 및 tailwind css를 사용하여 구축된 재사용 가능한 컴포넌트
+  - 라이브러리를 설치하여 사용하는게 아닌 원하는 컴포넌트를 복붙해서 사용하는 형식
+  - 자유로운 커스터마이징 가능 _(기획 요구사항)_
+  - 불필요한 번들링이 필요 없어짐
+  - 깔끔하고 세련된 디자인
+- **Tailwind CSS / SCSS**
+  - CSS 프레임워크 / CSS 전처리기
+  - 편리하고 빠른 개발 / 코드 가독성을 높이고 유지보수가 쉽고 효율적
+- **axios**
+  - http 통신을 위한 promise 기반의 자바스크립트 라이브러리
+  - 사용이 쉽고 익숙함
+- **dayjs**
+  - 날짜 계산 라이브러리
+  - 사이즈가 매우 작고 코드가 간편함
+- **exceljs**
+  - sheetjs에 비해 무료로 많은 기능을 지원
+- **lodash**
+  - 객체, 배열과 같은 데이터 구조를 간편하게 함수형으로 다룰 수 있음
+  - 지연평가 → 필요한 계산만 하여 성능 향상 효과
+  - 고차함수의 파라미터에 객체값도 줄 수 있다.
+- **react-uuid**
+  - 고유한 id값을 자동으로 추가해줌
+- **react-hook-form**
+  - React 기반의 form 관리 라이브러리
+  - 입력 필드의 값 변화만 추척 → 불필요한 렌더링 방지, 가상 DOM 업데이트 최소화
+  - 내장된 유효성 검사 지원 → 필수값, 에러 메세지 설정 가능
+- **zustand**
+  - store를 활용하여 전역 상태 관리 라이브러리, top-down 방식
+  - 사용 방법이 간결하고 가독성이 좋음
+  - store 형태임에도 간단하게 상태관리 구성 가능
+  - 일시적 업데이트 → 상태가 자주 바뀌더라도 매번 업데이트가 일어나지 않고 리렌더링을 제어하는 기능 탑재
 
-## Learn More
+---
 
-To learn more about Next.js, take a look at the following resources:
+## 폴더 구조
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+```bash
+common-admin/
+│
+├── config/                     # env 파일 (gitignore)
+│   └── .env
+│
+├── public/                     # 리소스들
+│   ├── fonts/
+│   ├── icons/
+│   ├── images/
+│
+├── src/
+│   ├── apis/                   # api와 관련된 로직
+│   ├── app/                    # 라우팅 관련 파일 정의
+│   │   └── page.tsx
+│   │   └── layout.tsx
+│   ├── components/             # 재사용 가능한 컴포넌트
+│   ├── hooks/                  # 커스텀 React 훅
+│   ├── lib/                    # 유틸리티 함수
+│   ├── stores/                 # 전역 상태 관리
+│   ├── styles/                 # 스타일 관련 파일(CSS, Tailwind 설정 등)
+│   └── types/                  # 타입
+│
+```
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,62 +1,76 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".gradient-grad": {
+          background:
+            "linear-gradient(84deg, rgba(11, 196, 9, 0.00) -99.58%, #0BC409 -70.18%, #06C8BB 110.41%)",
+        },
+        ".gradient-grad-hover": {
+          background:
+            "linear-gradient(84deg, rgba(0, 136, 0, 0.00) -99.58%, rgba(0, 136, 0, 0.25) -70.18%, #06877A 110.41%), linear-gradient(84deg, rgba(11, 196, 9, 0.00) -99.58%, #0BC409 -70.18%, #06C8BB 110.41%)",
+        },
+      });
+    }),
+  ],
 } satisfies Config;
