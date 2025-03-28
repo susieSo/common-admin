77d7560b48df6161626586825560917ae44aceed
소수지 <susie0529@imform.co.kr>
2025-03-28 16:45:44 +0900

    fix: data-table toggle button add toast

--- src/app/app/splash/(table)/tableRowAction.tsx
+++ src/app/app/splash/(table)/tableRowAction.tsx
@@ -3,10 +3,13 @@ import { Row } from "@tanstack/react-table";
 import { useDeleteTableData, useUpdateTableData } from "@/hooks/use-table-data";
 import { AlertPopup } from "@/components/Popup/alert-popup";
 import { Switch } from "@/components/ui/switch";
+import { useToast } from "@/hooks/use-toast";
 
 export const DataTableRowAction = ({ row }: { row: Row<Expense> }) => {
   const { mutate: deleteTableData } = useDeleteTableData();
   const { mutate: updateTableData } = useUpdateTableData();
+
+  const { addToast } = useToast();
 
   return (
     <div className="flex items-center gap-4">
@@ -19,12 +22,16 @@ export const DataTableRowAction = ({ row }: { row: Row<Expense> }) => {
       />
       <Switch
         checked={row.original.exposure}
-        onCheckedChange={() =>
+        onCheckedChange={() => {
+          addToast({
+            message: "데이터가 업데이트되었습니다.",
+            type: "success",
+          });
           updateTableData({
             id: row.original.id,
             exposure: !row.original.exposure,
-          })
-        }
+          });
+        }}
       />
     </div>
   );
